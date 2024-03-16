d4144a2bd87bb64a1e0d50e5e38077363bb4d446
han <rudgksdl94@gmail.com>
2024-03-17 00:56:06 +0900

    backtracking

---
+++ backtracking/BOJ_15666_N과M12.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+int arr[10];
+int ans[10];
+
+void func(int k){
+    if (k == m){
+        for (int i = 0; i < m; i++) cout << ans[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    int temp = 0;
+    for (int i = 0; i < n; i++) {
+        if (temp == arr[i] || ans[k -1] > arr[i]) continue;
+        ans[k] = arr[i];
+        temp = ans[k];
+        func(k + 1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    func(0);
+}
