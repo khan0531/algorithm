fa1c4750e309fbdb824b50482d90f87165086434
han <rudgksdl94@gmail.com>
2024-03-18 00:19:46 +0900

    kruscal

---
+++ kruscal/BOJ_9372_상근이이여행.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int t, n, m;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> t;
+    while(t--){
+        cin >> n >> m;
+
+        int a, b;
+        while(m--){
+            cin >> a >> b;
+        }
+        cout << n - 1 << '\n';
+    }
+}
