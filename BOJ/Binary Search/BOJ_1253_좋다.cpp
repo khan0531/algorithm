76daaff7ebfca9baab24b0de2cad16dc55964f27
han <rudgksdl94@gmail.com>
2024-03-19 18:28:30 +0900

    Binary Search

--- BOJ/Binary Search/BOJ_1253_좋다.cpp
+++ BOJ/Binary Search/BOJ_1253_좋다.cpp
@@ -1,34 +1,32 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
-int n;
-int a[2005];
-int cnt;
+int n, cnt;
+ll a[2005];
+bool is_used[2005];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> n;
+
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    sort(a, a + n);
 
+    sort(a, a + n);
+    
     for (int i = 0; i < n; i++) {
-        bool is_used = false;
         for (int j = 0; j < n; j++) {
-            if(i == j) continue;
-            int idx = lower_bound(a, a + n, a[i] - a[j]) - a;
-            if(is_used) break;
-            while(idx < n && a[idx] == a[i] - a[j]){
-                    if(idx != i && idx != j) {
-                        cnt++;
-                        is_used = true;
-                        break;
-                    }
-                idx++;
+            if (j == i) continue;
+            int lb = lower_bound(a, a + n, a[i] - a[j]) - a;
+            while (lb == i || lb == j) lb++;
+            if (lb < n && a[lb] == a[i] - a[j]) {
+                cnt++;
+                break;
             }
         }
     }
