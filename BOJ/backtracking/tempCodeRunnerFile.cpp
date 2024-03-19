817cc37acadeef9d986aec863f2fbf7bc1edb5e8
han <rudgksdl94@gmail.com>
2024-03-19 16:43:04 +0900

    second

--- BOJ/backtracking/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-          for (int i = 0; i < m; i++) {
-              cout << ans[i] << ' ';
-            } 
--- BOJ/bfs/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-
-    // for (int i = 0; i < N; i++) {
-    //     for (int j = 0; j < N; j++) {
-    //         cout << vis[i][j] << ' ';
-    //     }
-    //     cout << '\n';
-    // }
